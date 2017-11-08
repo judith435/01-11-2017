d720e116d43a8916b3e921d024474ed7a61457a8
judith435 <judithguttmann@gmail.com>
2017-11-08 21:27:46 +0200

    lesson 08112017

---
+++ app/filter.js
@@ -0,0 +1,37 @@
+john.filter("capitalize", function() {
+
+    return function(input) {
+
+        var results = [];
+        var output;
+        for (let i = 0; i < input.length; i++) { 
+            //input[i].name = input[i].name.toUpperCase();
+            output = input[i];
+            output.name = input[i].name.toUpperCase();
+            results.push(output);
+           // results.push(input[i]);
+        }
+        return results;
+    }
+
+
+});
+
+john.filter("bestseller", function() {
+    
+        return function(input, bestISBN) {
+    
+            for (let i = 0; i < input.length; i++) { 
+                if (input[i].isbn == bestISBN){
+                    input[i].isbn = input[i].isbn + "  >>> BEST SELLER <<<";
+                }
+                else {
+                    input[i].isbn = input[i].isbn.replace('  >>> BEST SELLER <<<','');;
+                }
+
+            }
+            return input;
+        }
+    
+    
+    });
