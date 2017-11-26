7711d7fed0dbb56c7d177f1fbb4e7d2482d188ab
judith435 <judithguttmann@gmail.com>
2017-11-26 07:59:54 +0200

    judy....

---
+++ judy/judy.js
@@ -0,0 +1,7 @@
+var Judith = angular.module('judy', []);//'judy' must be same name as  ng-app='judy'
+
+
+Judith.controller('calci', function Calc($scope)  {
+    $scope.numbi = 4;
+    $scope.myName = "Judith Ilson";
+});
