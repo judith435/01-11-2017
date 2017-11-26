0bf432f75ebe3fd25954bc8818aeab8714b9a0ea
judith435 <judithguttmann@gmail.com>
2017-11-26 22:10:08 +0200

    Update service.js

--- ls68/weather/service.js
+++ ls68/weather/service.js
@@ -1,5 +1,5 @@
 weatherApp.service('weatherService', function($http) {
-    const apiKey = 'e06a2f3d21ca934dc5a82ff8ba8ec0d8';
+    const apiKey = '';
 
     this.getDataByCityName = function(cityName, onSuccess, onError ) {
         $http({
