9590394c4b0da0a095374d2913c42a9a23c33690
Yehudit Ilson <yehuditi@one1.co.il>
2017-12-24 13:27:47 +0200

    save..

--- notes/controller.js
+++ notes/controller.js
@@ -15,18 +15,20 @@ notesApp.controller('ctrlAddNote', function notesCtrl($scope, noteService) {
     $scope.memo_time = new Date();
     
     $scope.addNote = function()  {
-        //if (validationsLogin.formValidated.contents.valid()){
-            note = {
-                date: $scope.memo_date,
-                time: $scope.memo_time,
-                content: $scope.memo_content,
-                new: true
-            };
-            noteService.saveNote(note);
-            $scope.memo_date = new Date();  
-            $scope.memo_time = new Date();
-            $scope.memo_content = "";
-       // }
+        if ($scope.formNewNote.$invalid){
+            alert("errori");
+            return;
+        }
+        note = {
+            date: $scope.memo_date,
+            time: $scope.memo_time,
+            content: $scope.memo_content,
+            new: true
+        };
+        noteService.saveNote(note);
+        $scope.memo_date = new Date();  
+        $scope.memo_time = new Date();
+        $scope.memo_content = "";
     }  
 });
 
